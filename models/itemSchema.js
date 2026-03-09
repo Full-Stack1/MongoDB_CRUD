b727501e6f1cbc06c5cf5df382f2985920ec1bf2
nooralrawashdah <nooralrawashdah5@gmail.com>
2026-03-10 00:37:58 +0300

    first commit

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -3,6 +3,11 @@ const itemschema= new mongoose.Schema
 ({ 
      name : {type: String , require : true , unique : true},
      image :{type : String},
-     category : {type : String , required: true , enum:["elect","clothing","healthy"]}
-})
+     category : {type : String , required: true , enum:["elect","clothing","healthy"]},
+     isDeleted:{type: Boolean,default:false}   //first way to soft delete
+},
+{
+    timestamps : true    //to add time the add the item
+}
+)
 module.exports =mongoose.model("Item",itemschema);
